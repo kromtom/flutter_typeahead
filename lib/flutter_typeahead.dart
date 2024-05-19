9e2fe00f50309ae171031e461acfceabeaf35515
Tomasz Kromrych <tkromrych@gmail.com>
2024-05-20 09:28:29 +1000

    Expose types

--- lib/flutter_typeahead.dart
+++ lib/flutter_typeahead.dart
@@ -4,3 +4,4 @@ export 'package:flutter_typeahead/src/common/base/suggestions_controller.dart';
 export 'package:flutter_typeahead/src/common/field/typeahead_field.dart';
 export 'package:flutter_typeahead/src/material/typeahead_field.dart';
 export 'package:flutter_typeahead/src/cupertino/typeahead_field.dart';
+export 'package:flutter_typeahead/src/common/base/types.dart';
